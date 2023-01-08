0cb5f3016aa281bab2df2f9d823c6dbcd03317f2
chiayuzhang <1201101003@student.mmu.edu.my>
2023-01-08 23:54:42 +0800

    submitPayment(done), paymentDetail(done)(both), submitReceipt(done)

--- cfo_submitPayment.php
+++ cfo_submitPayment.php
@@ -2,7 +2,28 @@
 <html>
 
     <style>
+        #textbox1, #textbox2 {
+            display: block;
+            float: left;    
+            width: 100px;    
+            height: 100px;    
+        }
 
+        .column{
+            float: left;
+            width: 60%;
+        }
+        
+        .column1{
+            float: right;
+            width: 73%;
+        }
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
     </style>
 
     <body>
@@ -13,7 +34,7 @@
         
     <form >
         <div id = "textbox"><center>
-            
+        
             <label for = "Invoice ID"> Invoice ID : </label>
             <input type = "text" id = "Invoice ID" name = "Invoice ID"> 
 
@@ -26,6 +47,7 @@
             <label for = "Payment Date"> Payment Date : </label>
             <input type = "date" id = "Payment Date" name = "Payment Date" > <br><br>
 
+            
             <label for = "Company Name"> Company Name : </label>
             <input type = "text" id = "Company Name" name = "Company Name">
 
@@ -34,7 +56,7 @@
         </center></div>
 
         <center>
-            <div>
+        <div class = "column1">
             <button type="cancel", name= "cancel payment"> Cancel </button>
             <button type="submit", name="save payment"> Submit Payment </button>
             </div>
@@ -43,3 +65,59 @@
 
     </body>
 </html>    
+
+
+        <!-- <form ><center>
+            <div id = "textbox1">
+                <label for = "Invoice ID"> Invoice ID : </label>
+                <input type = "text" id = "Invoice ID" name = "Invoice ID" maxlength = "16"> <br><br>
+
+                <label for = "Company ID"> Company ID : </label>
+                <input type = "text" id = "Company ID" name = "Company ID" maxlength = "16"> <br><br>
+
+                <label for = "Company Name"> Company Name : </label>
+                <input type = "text" id = "Company Name" name = "Company Name" maxlength = "16"> <br><br>
+            </div>
+
+            <div id = "textbox2">
+                <label for = "Payment ID"> Payment ID : </label>
+                <input type = "text" id = "Payment ID" name = "Payment ID" maxlength = "16"> <br><br>
+
+                <label for = "Payment Date"> Payment Date : </label>
+                <input type = "data" id = "Payment Date" name = "Payment Date" > <br><br>
+
+                <label for = "Payment Type"> Payment Type : </label>
+                <input type = "text" id = "Payment Type" name = "Payment Type" maxlength = "16"> <br><br>
+            </div>
+
+            <div>
+                <button type="cancel", name= "cancel payment"> Cancel </button>
+                <button type="submit", name="save payment"> Submit Payment </button>
+            </div>
+        </center>    
+        </form> -->
+
+        <!-- <div class = "row"><center>
+            <div class = "column">
+                <label for = "Invoice ID"> Invoice ID : </label>
+                <input type = "text" id = "Invoice ID" name = "Invoice ID"> 
+
+                <label for = "Company ID"> Company ID : </label>
+                <input type = "text" id = "Company ID" name = "Company ID" > <br><br>
+
+                <label for = "Company Name"> Company Name : </label>
+                <input type = "text" id = "Company Name" name = "Company Name"> <br><br>
+            </div>
+
+            <div class = "column">
+                <label for = "Payment ID"> Payment ID : </label>
+                <input type = "text" id = "Payment ID" name = "Payment ID" > <br><br>
+
+                <label for = "Payment Date"> Payment Date : </label>
+                <input type = "date" id = "Payment Date" name = "Payment Date" > <br><br>
+
+                <label for = "Payment Type"> Payment Type : </label>
+                <input type = "text" id = "Payment Type" name = "Payment Type"><br><br>
+            </div>  
+
+    </center></div> -->
