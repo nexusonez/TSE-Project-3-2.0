7cf94142016efac93511ec441cef5600c359d27c
nexusonez <asonkamlol88@gmail.com>
2023-01-15 17:41:19 +0800

    vendor page

---
+++ vendor_submitReceipt.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+    <style>
+        #textbox1, #textbox2 {
+            display: block;
+            float: left;    
+            width: 100px;    
+            height: 100px;    
+        }
+
+        .column{
+            float: left;
+            width: 84.3%;
+        }
+        
+        .column1{
+            float: right;
+            width: 73%;
+        }
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+    </style>
+
+    <body>
+
+        <h2><center>Please Enter Receipt Detail</center></h2>
+
+    <form >
+        <div id = "textbox"><center>
+            
+            <label for = "Invoice ID"> Invoice ID : </label>
+            <input type = "text" id = "Invoice ID" name = "Invoice ID"> 
+
+            <label for = "Payment ID"> Payment ID : </label>
+            <input type = "text" id = "Payment ID" name = "Payment ID" > <br><br>
+
+            <label for = "Company ID"> Company ID :</label>
+            <input type = "text" id = "Company ID" name = "Company ID">
+
+            <label for = "Payment Date"> Payment Date : </label>
+            <input type = "date" id = "Payment Date" name = "Payment Date" > <br><br>
+
+            <div class= "row">
+                <div class ="column"> 
+                    <label for = "Company Name"> Company Name : </label>
+                    <input type = "text" id = "Company Name" name = "Company Name"> <br><br>
+                </div>
+        </center></div>
+
+        <center>
+            <div class = "column1">
+            <button type="cancel", name= "cancel"> Cancel </button>
+            <button type="submit receipt", name= "submit receipt"> Submit Receipt </button>
+            </div>
+        </center>    
+        </form>  
+
+    </body>
+</html>    
