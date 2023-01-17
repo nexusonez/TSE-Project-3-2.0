e8100c04058b8e5e7f2f564f2188412d72fbd787
nexusonez <asonkamlol88@gmail.com>
2023-01-17 18:19:48 +0800

    Update vendor_submitInvoice.php

--- vendor_submitInvoice.php
+++ vendor_submitInvoice.php
@@ -10,7 +10,7 @@
         .grid-container {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(4, 1fr);
+        grid-template-rows: repeat(2, 1fr);
         grid-gap: 10px;
         padding-bottom: 5px;
         padding-right: 625px;
@@ -37,77 +37,38 @@
         <form action="" method="POST"><center>
             <div class="grid-container">
             <div class="grid-item">                
-                <label for = "Invoice ID"> Invoice ID : </label>
-            </div>
-            <div class="grid-item">
-                <input type = "text" id = "Invoice ID" name = "Invoice ID" maxlength = "10" required> 
-            </div>
-            <div class="grid-item">
                 <label for = "Company ID"> Company ID : </label>
-            </div>
-            <div class="grid-item">
-
-                <input type = "text" id = "Company ID" name = "Company ID" maxlength = "10" required> 
             </div>
             <div class="grid-item">
                 <label for = "Company Name"> Company Name : </label>
             </div>
             <div class="grid-item">
+                <label for = "Invoice ID"> Invoice ID : </label>
+            </div>
+            <div class="grid-item">
+                <label for = "Invoice Due"> Invoice Due : </label>
+            </div>
+
+            <div class="grid-item">
+                <input type = "text" id = "Company ID" name = "Company ID" maxlength = "10" required> 
+            </div>
+            <div class="grid-item">
                 <input type = "text" id = "Company Name" name = "Company Name" maxlength = "255" required> 
             </div>
             <div class="grid-item">
-                <label for = "Payment ID"> Payment ID : </label>
+                <input type = "text" id = "Invoice ID" name = "Invoice ID" maxlength = "10" required> 
             </div>
             <div class="grid-item">
-                <input type = "text" id = "Payment ID" name = "Payment ID" maxlength = "100" required> 
+                <input type = "date" id = "Invoice Due" name = "Invoice Due" required> 
             </div>
-            <div class="grid-item">
-                <label for = "Payment Date"> Payment Date : </label>
-            </div>
-            <div class="grid-item">
-                <input type = "date" id = "Payment Date" name = "Payment Date" required>
-            </div>
-            <div class="grid-item">
-                <label for = "Payment Type"> Payment Type : </label>
-                </div>
-                <div class="grid-item">
-                    <select required class="form-input" id = "Payment Type" name = "Payment Type" width = "16" required> 
-                        <option value="Card">Card</option>
-                        <option value="Cash">Cash</option>
-                        <option value="Online">Online</option>
-                    </select>
-                </div>
 
-            <br>
-                </div>
+            
                 <div>
-                    <button type="cancel", name= "cancel payment"><a class= "a" href="cfo_home.php"> Cancel </a></button>
-                    <button type="submit", name="save payment"> Submit Payment </button>
+                    <button type="cancel", name= "cancel payment"><a class= "a" href="vendor_home.php"> Cancel </a></button>
+                    <button type="submit", name="save payment"> Submit Invoice </button>
                 </div>
         </center>    
         </form>
     </body>
 </html>    
 
-
-
-<!-- 
-<form ><center>
-            <label for = "Invoice ID"> Invoice ID : </label>
-            <input type = "text" id = "Invoice ID" name = "Invoice ID" maxlength = "16"> <br><br>
-            <label for = "Company ID"> Company ID : </label>
-            <input type = "text" id = "Company ID" name = "Company ID" maxlength = "16"> <br><br>
-            <label for = "Company Name"> Company Name : </label>
-            <input type = "text" id = "Company Name" name = "Company Name" maxlength = "16"> <br><br>
-            <label for = "Payment ID"> Payment ID : </label>
-            <input type = "text" id = "Payment ID" name = "Payment ID" maxlength = "16"> <br><br>
-            <label for = "Payment Date"> Payment Date : </label>
-            <input type = "data" id = "Payment Date" name = "Payment Date" > <br><br>
-            <label for = "Payment Type"> Payment Type : </label>
-            <input type = "text" id = "Payment Type" name = "Payment Type" maxlength = "16"> <br><br>
-            <div>
-            <button type="cancel", name= "cancel payment"> Cancel </button>
-            <button type="submit", name="save payment"> Submit Payment </button>
-            </div>
-        </center>    
-        </form>   -->
